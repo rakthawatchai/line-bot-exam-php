2fb2a00f756418d896483eabfa7067f88bdf83a2
thawatchai <rak.thawatchai@gmail.com>
2020-03-14 20:13:33 +0700

    profile

--- profile.php
+++ profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'HxIzkqvwztsi0nd2P4n11oQeXrPyB99Qz5/PNQ79U2GlB0856crrxtW8kRuED2Srml0pci4pIZQ2TDR2YfAhD8tlp5P7wdIUgeSnRSqV0mho11Az0HYMHnrbxsLMocmPQTzcaLTn7ZZ14BgTBYuddQdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'Ub06b3f2ec634f0d1ee5e754c98f9dbdc';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
@@ -17,4 +17,3 @@ $result = curl_exec($ch);
 curl_close($ch);
 
 echo $result;
-
